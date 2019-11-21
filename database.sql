f484e9be696df81b40ae03bbc600602a054d662d
kevin <wei.liu@ifchange.com>
2019-11-21 11:47:53 +0800

    add database

---
+++ database.sql
@@ -0,0 +1,4 @@
+-- utf8
+CREATE DATABASE `database_name` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- GBK
+CREATE DATABASE `database_name` DEFAULT CHARACTER SET gbk COLLATE gbk_chinese_ci;
